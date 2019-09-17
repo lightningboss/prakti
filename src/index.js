463b8aa77dc7f2a4ac95b0d8887399b010bcc933
Marc Nitzsche <marc.nitzsche@live.de>
2019-09-17 13:37:43 +0200

    Add user-select: none

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import 'typeface-roboto';
 
 import App from './App';
 import { registerServiceWorker } from './lib/serviceWorker';
+import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
